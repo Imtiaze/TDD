9c60f63cd62a395a7bdd097f3a3706edd5122687
imtiaze_techvill <imtiaze.techvill@gmail.com>
2024-09-21 21:16:32 +0600

    Unique department store test complete

--- test_driven_api__with_laravel_project/database/migrations/2024_09_12_131740_create_departments_table.php
+++ test_driven_api__with_laravel_project/database/migrations/2024_09_12_131740_create_departments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->uuid('uuid');
             $table->string('name')->unique();
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
--- test_driven_api__with_laravel_project/tests/Unit/Department/CreateDepartmentTest.php
+++ test_driven_api__with_laravel_project/tests/Unit/Department/CreateDepartmentTest.php
@@ -1,16 +1,34 @@
 <?php
 
+use App\Models\Department;
+
 use function Pest\Laravel\postJson;
 
 it('should create a department', function() {
     $department = postJson(route('departments.store'), [
-        'name' => 'asdfasdf',
+        'name' => 'Development',
         'description' => 'Awesome developers accross the board',
     ])->json('data');
 
     expect($department)
-        ->attributes->name->toBe('asdfasdf')
+        ->attributes->name->toBe('Development')
         ->attributes->description->toBe('Awesome developers accross the board');
     
 });
 
+
+it('should return 422 if name is invalid', function(?string $name) {
+    Department::factory([
+        'name' => 'development',
+    ])->create();
+
+    postJson(route('departments.store'), [
+        'name' => $name,
+        'description' => 'description'
+    ])->assertInvalid(['name']); 
+
+})->with([
+    '',
+    null,
+    'development'
+]);
